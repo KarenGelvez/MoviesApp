21efb4b11daaa592b42aaf50f395ccb0f33b4a29
KarenGelvez <karen.gelvez2@unipamplona.edu.co>
2021-04-27 10:02:08 -0500

    [ADD + RFT] interface MoviesState + peticiones de forma simultanea

--- src/hooks/useMovies.tsx
+++ src/hooks/useMovies.tsx
@@ -1,19 +1,49 @@
 import {useEffect, useState} from 'react';
 import movieDB from '../api/movieDB';
-import {MovieDBNowPlaying, Movie} from '../interfaces/IMovie';
+import {MovieDBMoviesResponse, Movie} from '../interfaces/IMovie';
+
+interface MoviesState {
+  nowPlaying: Movie[];
+  popular: Movie[];
+  topRated: Movie[];
+  upcoming: Movie[];
+}
 
 export const useMovies = () => {
-  const [moviesNowCinema, setmoviesNowCinema] = useState<Movie[]>([]);
+  const [moviesState, setMoviesState] = useState<MoviesState>({
+    nowPlaying: [],
+    popular: [],
+    topRated: [],
+    upcoming: [],
+  });
   const [loading, setloading] = useState(true);
+
   const getMovies = async () => {
-    const resp = await movieDB.get<MovieDBNowPlaying>('/now_playing');
-    const movies = resp.data.results;
-    setmoviesNowCinema(movies);
+    const nowPlayingPromise = movieDB.get<MovieDBMoviesResponse>(
+      '/now_playing',
+    );
+    const popularPromise = movieDB.get<MovieDBMoviesResponse>('/popular');
+    const topRatedPromise = movieDB.get<MovieDBMoviesResponse>('/top_rated');
+    const upcomingPromise = movieDB.get<MovieDBMoviesResponse>('/upcoming');
+
+    const resp = await Promise.all([
+      nowPlayingPromise,
+      popularPromise,
+      topRatedPromise,
+      upcomingPromise,
+    ]);
+
+    setMoviesState({
+      nowPlaying: resp[0].data.results,
+      popular: resp[1].data.results,
+      topRated: resp[2].data.results,
+      upcoming: resp[3].data.results,
+    });
     setloading(false);
   };
 
   useEffect(() => {
     getMovies();
   }, []);
-  return {moviesNowCinema, loading};
+  return {...moviesState, loading};
 };
