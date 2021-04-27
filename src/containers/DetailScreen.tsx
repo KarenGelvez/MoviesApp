d711eb1170033a277b0285abd748bfd064da259d
KarenGelvez <karen.gelvez2@unipamplona.edu.co>
2021-04-27 16:49:22 -0500

    [ADD] primeros componentes de la pantalla

--- src/containers/DetailScreen.tsx
+++ src/containers/DetailScreen.tsx
@@ -1,10 +1,75 @@
+import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Image,
+  Dimensions,
+  ScrollView,
+  Text,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {RootStackParams} from '../navigation/StackNavigator';
 
-export const DetailScreen = () => {
+interface IProps extends StackScreenProps<RootStackParams, 'Detail'> {}
+
+const {height} = Dimensions.get('screen');
+export const DetailScreen = ({route}: IProps) => {
+  const movie = route.params;
+  const uri = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+
   return (
-    <View>
-      <Text>Detail Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.wrapper}>
+        <View style={styles.imageBorder}>
+          <Image source={{uri}} style={styles.image} />
+        </View>
+      </View>
+      <View style={styles.marginContainer}>
+        <Text style={styles.subTitle}>{movie.original_title}</Text>
+        <Text style={styles.title}>{movie.title}</Text>
+      </View>
+      <View>
+        <Icon name="star-outline" size={20} color="gray" />
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    width: '100%',
+    height: height * 0.7,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.24,
+    shadowRadius: 7,
+    elevation: 9,
+    borderBottomEndRadius: 25,
+    borderBottomStartRadius: 25,
+  },
+  imageBorder: {
+    flex: 1,
+    overflow: 'hidden',
+    borderBottomEndRadius: 25,
+    borderBottomStartRadius: 25,
+  },
+  image: {
+    flex: 1,
+  },
+  marginContainer: {
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  subTitle: {
+    fontSize: 16,
+    opacity: 0.8,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
